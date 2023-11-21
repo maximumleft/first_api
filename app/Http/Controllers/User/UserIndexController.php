0f0709e0a7bc194bb24843f52f7d64cc78333d8a
Maksim <mdlevsha@gmail.com>
2023-11-21 15:29:12 +0300

    User

--- app/Http/Controllers/User/UserIndexController.php
+++ app/Http/Controllers/User/UserIndexController.php
@@ -3,11 +3,17 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\UserRequest;
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class UserIndexController extends Controller
 {
-    public function __invoke()
+    public function create(UserRequest $request)
     {
+        return User::create($request->validated());
 
+       // return view("wel");
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,3 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::post('/',[UserIndexController::class]);
