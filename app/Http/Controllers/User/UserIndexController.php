57ebceb5d08e47688fabde20fdc4f06fbd1ab98c
Maksim <mdlevsha@gmail.com>
2023-11-21 14:47:13 +0300

    test

---
+++ app/Http/Controllers/User/UserIndexController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
+
+class UserIndexController extends Controller
+{
+    public function __invoke()
+    {
+
+    }
+}
